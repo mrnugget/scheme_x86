79b7845ce3e589cf0e424e6f6880f96f7d7e6041
Thorsten Ball <mrnugget@gmail.com>
2019-08-02 07:37:58 +0200

    Add more tests for add1

--- src/tests/test-unary-primitives.scm
+++ src/tests/test-unary-primitives.scm
@@ -1,2 +1,7 @@
 (add-tests-with-string-output "prim-apply add1"
-  [(prim-apply add1 1)  => "2\n"])
+  [(prim-apply add1 1)  => "2\n"]
+  [(prim-apply add1 0)  => "1\n"]
+  [(prim-apply add1 -1)  => "0\n"]
+  [(prim-apply add1 536870910) => "536870911\n"]
+  [(prim-apply add1 -536870912) => "-536870911\n"]
+  [(prim-apply add1 (prim-apply add1 0)) => "2\n"])
