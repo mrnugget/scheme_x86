a59d134cd24f08fb49a72233004af226a7e3e7a1
Thorsten Ball <mrnugget@gmail.com>
2019-08-03 14:34:56 +0200

    Add more tests for let expressions

--- src/tests/test-let-expressions.scm
+++ src/tests/test-let-expressions.scm
@@ -1,3 +1,6 @@
 (add-tests-with-string-output "let expressions"
   [(let ([x 3]) x)  => "3\n"]
-  [(let ([x 3] [y 5]) (prim-apply + x y))  => "8\n"])
+  [(let ([x 3] [y 5]) (prim-apply + x y))  => "8\n"]
+  [(let ([x (prim-apply + 1 2)]) x) => "3\n"]
+  [(let ([x (prim-apply + 1 2)]) (let ([y (prim-apply + x 3)]) y)) => "6\n"]
+  [(let ([x (prim-apply + 1 2)]) (let ([x (prim-apply + 3 3)]) x)) => "6\n"])
