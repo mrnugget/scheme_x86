8a23cb19059271ee9e4adb2724241d6d0733e749
Thorsten Ball <mrnugget@gmail.com>
2019-09-01 20:07:06 +0200

    Remove test for primitive that's not implemented

--- src/tests/test-let-expressions.scm
+++ src/tests/test-let-expressions.scm
@@ -5,7 +5,6 @@
   [(let ([x 3] [y 5]) (prim-apply + 30 30) (prim-apply + 10 10))  => "20\n"]
   [(let ([x 3] [y 5]) (prim-apply + x y) (prim-apply + x 9))  => "12\n"]
   [(let ([x 3] [y 5]) (prim-apply + x y) (prim-apply + x 97))  => "100\n"]
-  [(let ([x 3] [y 5]) (prim-apply + x y) (prim-apply - x 4))  => "1\n"]
   [(let ([x (prim-apply + 1 2)]) x) => "3\n"]
   [(let ([x (prim-apply + 1 2)]) (let ([y (prim-apply + x 3)]) y)) => "6\n"]
   [(let ([x (prim-apply + 1 2)]) (let ([x (prim-apply + 3 3)]) x)) => "6\n"])
