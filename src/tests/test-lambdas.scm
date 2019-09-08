ff1073508513c361138106bc174f134238b43f1f
Thorsten Ball <mrnugget@gmail.com>
2019-09-08 11:08:00 +0200

    Reorganize lambdas tests

--- src/tests/test-lambdas.scm
+++ src/tests/test-lambdas.scm
@@ -1,14 +1,32 @@
 (add-tests-with-string-output "lambdas"
   [((lambda (x) x) 99) => "99\n"]
+  [((lambda (x)
+      (prim-apply + 1 x))
+    2) => "3\n"]
+  [((lambda (x y)
+      (prim-apply + x y))
+    2
+    3) => "5\n"]
+  [((lambda (a b c d e)
+      (prim-apply +
+                  a
+                  (prim-apply +
+                              b
+                              (prim-apply +
+                                          c
+                                          (prim-apply + d e)))))
+    1 2 3 4 5) => "15\n"]
+
   [(lambda (x) x) => "<closure>\n"]
+
   [(prim-apply closure? (lambda (x) x)) => "#t\n"]
   [(prim-apply closure? 1) => "#f\n"]
   [(prim-apply closure? (prim-apply make-vector 0)) => "#f\n"]
   [(prim-apply closure? (prim-apply make-string 0)) => "#f\n"]
-  [((lambda (x) (prim-apply + 1 x)) 2) => "3\n"]
-  [((lambda (x y) (prim-apply + x y)) 2 3) => "5\n"]
-  [((lambda (a b c d e) (prim-apply + a (prim-apply + b (prim-apply + c (prim-apply + d e))))) 1 2 3 4 5) => "15\n"]
+
   [((lambda (g) (g)) (lambda () 99)) => "99\n"]
   [(let ([f (lambda (g) (g 2 13))])
     (f (lambda (n m) (prim-apply + n m)))) => "15\n"]
-  [((lambda (g x) (g x)) (lambda (x) (prim-apply + 1 x)) 2) => "3\n"])
+  [((lambda (g x) (g x)) (lambda (x) (prim-apply + 1 x)) 2) => "3\n"]
+  [(let ([f (lambda (g) (prim-apply + (g 10) (g 100)))])
+    (f (lambda (x) (prim-apply + x x)))) => "220\n"])
