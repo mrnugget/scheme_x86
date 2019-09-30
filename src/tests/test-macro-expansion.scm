8ed65f67c2dd75a2324fb8aff906568c2a3e7249
Thorsten Ball <mrnugget@gmail.com>
2019-09-30 19:37:56 +0200

    Add more tests for macro-expansion of let*

--- src/tests/test-macro-expansion.scm
+++ src/tests/test-macro-expansion.scm
@@ -1,10 +1,24 @@
 (add-tests-with-precompiled-output "let* to let"
-  [(let* ([x 1]
-          [y 2])
-     (prim-apply + x y))
-  => (labels
-       ()
-       ()
-       (let ((x 1))
-         (let ((y 2))
-           (prim-apply + x y))))])
+  [(let* ([x 1]) (prim-apply + x y))
+   => (labels
+        ()
+        ()
+        (let ((x 1))
+          (prim-apply + x y)))]
+
+  [(let* ([x 1] [y 2]) (prim-apply + x y))
+   => (labels
+        ()
+        ()
+        (let ((x 1))
+          (let ((y 2))
+            (prim-apply + x y))))]
+
+  [(let* ([result (let* ([x 1] [y 2]) (prim-apply + x y))]
+          [z 3])
+     (prim-apply + result z))
+   => (labels
+        ()
+        ()
+        (let ([result (let ([x 1]) (let ([y 2]) (prim-apply + x y)))])
+          (let ([z 3]) (prim-apply + result z))))])
