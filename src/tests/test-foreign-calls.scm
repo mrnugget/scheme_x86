6ae6d618c49d89dc009a8c7eee2d832aeda3c579
Thorsten Ball <mrnugget@gmail.com>
2019-10-10 08:15:18 +0200

    Add more tests for foreign calls

--- src/tests/test-foreign-calls.scm
+++ src/tests/test-foreign-calls.scm
@@ -1,4 +1,15 @@
 (add-tests-with-string-output "foreign calls"
  [(foreign-call "hello_from_c") => "Hello from C!\n()\n"]
  [(foreign-call "add_two" 2) => "4\n"]
+
+ [(let ((x (foreign-call "add_two" 2))
+        (y (foreign-call "add_two" 4)))
+    (prim-apply + x y)) => "10\n"]
+
+ [(let ((f (lambda (x y)
+             (prim-apply +
+                         (foreign-call "add_two" x)
+                         (foreign-call "add_two" y)))))
+    (f 2 4)) => "10\n"]
+
  [(foreign-call "print_three_args" 55 66 77) => "arg1=55, arg2=66, arg3=77\n()\n"])
